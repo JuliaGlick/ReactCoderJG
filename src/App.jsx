57ddedaf2787e396f8458b321fc1d136a27451d7
JuliaGlick <96437541+JuliaGlick@users.noreply.github.com>
2023-09-07 13:04:50 -0300

    firebase y local env

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css';
-import NavBar from './components/Navigation/Navigation';
+import Navigation from './components/Navigation/Navigation';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import { useEffect } from 'react';
+import { firestore } from './firebase/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
 
 
 
 function App() {
 
-  
+  useEffect(() => {
+    const collectionRef = collection(firestore, "products")
+    
+    getDocs(collectionRef)
+    .then((snapshot) => {
+      console.log({snapshot})
+      snapshot.forEach((doc) => console.log(doc.data()))
+  })
+    .catch((error) => console.error(error))
+  }, [])
+
   return (
     <>
       <ItemListContainer greeting={"Bienvenidos al Mejor Sitio de Venta de Ramen"}/>
@@ -14,4 +28,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
